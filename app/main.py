7036adb6d4a04a48b6ef4fc8e61b818c58279cc2
Samuel Sunday <sammysunnycr7@gmail.com>
2026-03-13 22:18:38 +0100

    Slight changes to the main file.

--- app/main.py
+++ app/main.py
@@ -29,4 +29,4 @@ app.include_router(votes.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to my API"}
+    return {"message": "Welcome to my API. Rate my work on a scale of 10."}
